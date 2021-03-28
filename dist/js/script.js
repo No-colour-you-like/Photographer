a34daa5732b1818acec23964c6631500a399e6f6
Nikita <no.colour.you.like@gmail.com>
2021-03-28 16:19:27 +0300

    menu complete

--- dist/js/script.js
+++ dist/js/script.js
@@ -14,3 +14,31 @@ navDots.forEach(dot => {
     scrollToBlock(secondBlock)
   })
 });
+
+//Menu open
+
+const menuBtn = document.querySelector('.header__menu-name'),
+ mainMenu = document.querySelector('.menu');
+
+const openBlockMenu = () => {
+  mainMenu.classList.toggle('display-flex')
+}
+
+const openSlowMenu = () => {
+  mainMenu.classList.toggle('opacity-show')
+}
+
+menuBtn.addEventListener('click', () => {
+  
+  
+
+  if (!mainMenu.classList.contains('display-flex')) {
+    openBlockMenu()
+    setTimeout(openSlowMenu, 100)
+  } else {
+    openSlowMenu()
+    setTimeout(openBlockMenu, 1100)
+  }
+
+});
+
