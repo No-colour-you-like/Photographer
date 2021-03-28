d141517544b0e88f05d5a302a35bd1293ebeb4a0
Nikita <no.colour.you.like@gmail.com>
2021-03-28 20:33:04 +0300

    about process

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,10 @@
+
 const navDots = document.querySelectorAll('.right-panel__dot'),
-  secondBlock = document.querySelector('.second');
+  firstBlock = document.querySelector('.first'),
+  secondBlock = document.querySelector('.second'),
+  thirdBlock = document.querySelector('.third'),
+  fourthBlock = document.querySelector('.fourth'),
+  fifthBlock = document.querySelector('.fifth');
 
 
 let scrollToBlock = (block) => {
@@ -9,9 +14,66 @@ let scrollToBlock = (block) => {
   });
 };
 
-navDots.forEach(dot => {
+//Change header and footer color scroll
+
+
+
+
+// document.addEventListener('wheel', (e) => {
+
+//   if (e.deltaY < 0) {
+//     scrollToBlock(firstBlock)
+//   } else if (e.deltaY > 0) {
+//     scrollToBlock(secondBlock)
+//   }
+
+// })
+
+const headerMenuName = document.querySelector('.header__menu'),
+headerLine = document.querySelector('.header__menu-line');
+const blocks = [firstBlock, secondBlock, thirdBlock, fourthBlock, fifthBlock]
+
+navDots.forEach((dot, i) => {
   dot.addEventListener('click', () => {
-    scrollToBlock(secondBlock)
+
+    blocks.forEach(item => {
+      item.classList.remove('active')
+    });
+
+    switch(i) {
+      case 0:
+        scrollToBlock(firstBlock)
+        firstBlock.classList.add('active')
+        break
+      case 1:
+        scrollToBlock(secondBlock)
+        secondBlock.classList.add('active')
+        break
+      case 2:
+        scrollToBlock(thirdBlock)
+        thirdBlock.classList.add('active')
+        break
+      case 3:
+        scrollToBlock(fourthBlock)
+        fourthBlock.classList.add('active')
+        break
+      case 4:
+        scrollToBlock(fifthBlock)
+        fifthBlock.classList.add('active')
+        break
+    }
+
+    if (firstBlock.classList.contains('active')) {
+      headerMenuName.classList.remove('red-color')
+      headerMenuName.classList.add('grey-color')
+      headerLine.classList.remove('red-background')
+      headerLine.classList.add('grey-background')
+    } else  {
+      headerMenuName.classList.remove('grey-color')
+      headerMenuName.classList.add('red-color')
+      headerLine.classList.remove('grey-background')
+      headerLine.classList.add('red-background')
+    }
   })
 });
 
@@ -30,15 +92,44 @@ const openSlowMenu = () => {
 
 menuBtn.addEventListener('click', () => {
   
-  
-
   if (!mainMenu.classList.contains('display-flex')) {
     openBlockMenu()
     setTimeout(openSlowMenu, 100)
   } else {
     openSlowMenu()
-    setTimeout(openBlockMenu, 1100)
+    setTimeout(openBlockMenu, 800)
   }
-
+  
 });
 
+
+
+let blockHeight = getComputedStyle(firstBlock).height
+
+// const btn = document.querySelector('.right-panel__active');
+// blockHeight = blockHeight.slice(0, -2)
+
+// document.addEventListener('click', () => {
+  
+//   window.scrollBy({
+//     top: blockHeight,
+//     behavior: 'smooth'
+//   })
+// });
+
+//About btn open text 
+
+const secondPhoto = document.querySelector('.second__photo'),
+aboutBtn = document.querySelector('.about-see-btn');
+
+aboutBtn.addEventListener('click', () => {
+ 
+  if (!secondPhoto.classList.contains('second-photo-open')) {
+    secondPhoto.classList.remove('second-photo-hide')
+    secondPhoto.classList.add('second-photo-open')
+  } else {
+    secondPhoto.classList.remove('second-photo-open')
+    secondPhoto.classList.add('second-photo-hide')
+  }
+  
+});
