a6a23b0bcf397da6ec6213fd6d5fa21ee4ea2835
Nikita <no.colour.you.like@gmail.com>
2021-03-29 19:40:19 +0300

    menu process

--- dist/js/script.js
+++ dist/js/script.js
@@ -40,8 +40,11 @@ navDots.forEach((dot, i) => {
 
 //Main menu open
 
-const headerMenuName = document.querySelector('.header__menu'),
+const headerMenu = document.querySelector('.header__menu'),
+headerMenuName = document.querySelector('.header__menu-name'),
 headerLine = document.querySelector('.header__menu-line'),
+headerItem = document.querySelector('.header__menu-item'),
+menuLeftBlock = document.querySelector('.menu__left-block'),
 footerLinks = document.querySelectorAll('.footer__link-icon');
 
 const menuBtn = document.querySelector('.header__menu-name'),
@@ -55,19 +58,33 @@ const openSlowMenu = () => {
   mainMenu.classList.toggle('opacity-show');
 };
 
+const openLeftBlock = () => {
+  menuLeftBlock.classList.add('transform-left-block')
+}
+
 menuBtn.addEventListener('click', () => {
   
+
   if (!(mainBlock.scrollTop >= blockHeight)) {
-    headerMenuName.classList.toggle('red-color')
+    headerMenu.classList.toggle('red-color')
+    headerMenuName.classList.toggle('transform-menu-name')
     headerLine.classList.toggle('red-background')
+    headerItem.classList.toggle('visibility-hidden')
     footerLinks.forEach(link => {
       link.classList.toggle('red-fill')
     })
   } 
   
+  if (headerMenuName.innerHTML === 'Меню') {
+    headerMenuName.innerHTML = 'Закрыть'
+  } else {
+    headerMenuName.innerHTML = 'Меню'
+  }
+
   if (!mainMenu.classList.contains('display-flex')) {
     openBlockMenu();
     setTimeout(openSlowMenu, 100);
+    setTimeout(openLeftBlock, 300)
   } else {
     openSlowMenu();
     setTimeout(openBlockMenu, 800);
@@ -96,19 +113,20 @@ aboutBtn.addEventListener('click', () => {
 
 //Change color width scroll
 
-const mainBlock = document.querySelector('.main');
+const mainBlock = document.querySelector('.main'),
+asideCounter = document.querySelector('.right-panel__active');
 let blockHeight = getComputedStyle(firstBlock).height.slice(0, -2)
 
   mainBlock.addEventListener('scroll', () => {
 
     if (mainBlock.scrollTop >= blockHeight) {
-      headerMenuName.classList.add('red-color')
+      headerMenu.classList.add('red-color')
       headerLine.classList.add('red-background')
       footerLinks.forEach(link => {
         link.classList.add('red-fill')
       })
-    } else {
-      headerMenuName.classList.remove('red-color')
+    } else if (mainBlock.scrollTop <= blockHeight && !mainMenu.classList.contains('display-flex')) {
+      headerMenu.classList.remove('red-color')
       headerLine.classList.remove('red-background')
       footerLinks.forEach(link => {
         link.classList.remove('red-fill')
@@ -120,18 +138,23 @@ let blockHeight = getComputedStyle(firstBlock).height.slice(0, -2)
       if (mainBlock.scrollTop < blockHeight) {
         dot.classList.remove('red-background-dot')
         navDots[0].classList.add('red-background-dot')
+        asideCounter.innerHTML = '01'
       } else if (mainBlock.scrollTop >= blockHeight && mainBlock.scrollTop < (blockHeight * 2)) {
         dot.classList.remove('red-background-dot')
         navDots[1].classList.add('red-background-dot')
+        asideCounter.innerHTML = '02'
       } else if (mainBlock.scrollTop >= (blockHeight * 2) && mainBlock.scrollTop < (blockHeight * 3)) {
         dot.classList.remove('red-background-dot')
         navDots[2].classList.add('red-background-dot')
+        asideCounter.innerHTML = '03'
       } else if (mainBlock.scrollTop >= (blockHeight * 3) && mainBlock.scrollTop < (blockHeight * 4)) {
         dot.classList.remove('red-background-dot')
         navDots[3].classList.add('red-background-dot')
+        asideCounter.innerHTML = '04'
       } else if (mainBlock.scrollTop >= (blockHeight * 4) && mainBlock.scrollTop < (blockHeight * 5)) {
         dot.classList.remove('red-background-dot')
         navDots[4].classList.add('red-background-dot')
+        asideCounter.innerHTML = '05'
       }
     })
   });
